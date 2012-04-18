04c354f5370b22696fa2fe5c8f237ef3750cc0c5
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-04-18 04:35:34 +0000

    Embred Experiment 1 exercise 1/3 finished. quite good... need to add the interrupt interrupt routine.

--- trunk/QianRuShi/exp2/main.c
+++ trunk/QianRuShi/exp2/main.c
@@ -3,15 +3,15 @@
 
 //Global define switches to control the program behavior to satified the needs.
 #define USE_MODULE1 0 
-#define USE_MODULE2 1
-#define USE_MODULE3 0
+#define USE_MODULE2 0
+#define USE_MODULE3 1
 
 #if ( USE_MODULE1 + USE_MODULE2 + USE_MODULE3 ) > 1 || ( USE_MODULE1 + USE_MODULE2 + USE_MODULE3 ) == 0
-#error "error of switches. Try to modify the USE_MODULE1 or USE_MODULE2 or USE_MODULE3" // 001 010 100 is acceptable
+#error "error of switches. Try to modify the USE_MODULE1 or USE_MODULE2 or USE_MODULE3" //only 001 010 100 is acceptable
 #endif
 
+
 #define LED_PORT1 P1
-#define DEAY_MS 150
 
 
 //delay function, no timer engaged which means it's inaccurate.
@@ -20,6 +20,8 @@ void delay( unsigned int ucMs );
 
 //module 1 entrance
 #if USE_MODULE1
+
+#define DEAY_MS 150
 
 int main( void )
 {
@@ -68,18 +70,19 @@ int main( void )
 //module 2 entrance
 #if USE_MODULE2
 
-bit mode;                  //evil global variable, indicate the mode we use here
-unsigned long ledStatus;   //We handle with care, But is there a risk of race like that in Operaton System?
+#define DEAY_MS 250
+
+volatile bit mode;                  //evil global variable, indicate the mode we use here
+volatile unsigned long ledStatus;   //We handle with care, But is there a risk of race like that in Operaton System?
 
 int main( void )
 {
 	//choose the external trigger mode
-	//IT1 = 0; //Can we use bit addressing? It's 88H. Now that int0 int1 are edage trigger
+	//Can we use bit addressing? It's 88H. Now that int0 edage trigger
 	IT0 = 1; //Try it. Man
 
 	//external interrupt enable
 	EX0 = 1; //external 0 allowed
-	//EX1 = 1; //external 1 allowed
 
 	//global interrupt enable
 	EA = 1;  //Global interrupt on!
@@ -106,24 +109,86 @@ int main( void )
 }
 
 #endif
-	
 
 
+#if USE_MODULE3
 
-//a delay function
-void delay( unsigned int ucMs )
+#define DEAY_MS 180
+
+
+volatile unsigned char ledStatus;    //change the ledStatus
+volatile mode;                       //has been interrupted?
+
+int main( void )
 {
-//running at 11.0592MHz ?
-#define DELAYTIMES 239 
 
-	unsigned char ucCounter;
+	unsigned char cycle;
 
-	while( ucMs != 0 )
+	//off all the leds
+	P0 = 0xFF;
+	P2 = 0xFF;
+	LED_PORT1 = 0xFF;
+
+	//choose the external trigger mode
+	//Can we use bit addressing? It's 88H. Now that int1 edage trigger
+	IT1 = 1; //Try it. Man
+
+	//external interrupt enable
+	EX1 = 1; //external 1 allowed
+
+	//global interrupt enable
+	EA = 1;  //Global interrupt on!
+
+	mode = 0;
+
+	while( 1 )
 	{
-		for( ucCounter=0; ucCounter < DELAYTIMES ; ucCounter ++ ){}
-		ucMs--;
+		if( ! mode )
+		{
+			ledStatus = 0xFF;
+			P0 = 0xFF;
+			P2 = 0xFF;
+
+			LED_PORT1 = ~ledStatus;
+			delay( DEAY_MS );
+
+			for( cycle=0 ; cycle < 8 ; cycle++ )
+			{
+				ledStatus >>= 1;
+				LED_PORT1 = ~ledStatus;
+				delay( DEAY_MS );
+			}
+		}
+		else{
+
+			ledStatus = 0x03;
+			LED_PORT1 = ~ledStatus;
+			P0 = 0xF0;
+			P2 = 0xFF;
+			delay( DEAY_MS );
+
+			for( cycle=0 ; cycle < 4 ; cycle++ )
+			{
+				ledStatus <<=2;
+
+				LED_PORT1 = ~ledStatus;
+				P0 = ~P0;
+				P2 = ~P2;
+				delay( DEAY_MS );
+			}
+
+
+		}
+
 	}
+
+
+	return 0;
+
 }
+
+#endif
+	
 
 
 #if USE_MODULE1
@@ -146,7 +211,7 @@ void ext0( void ) interrupt 0
 	EA = 1; //interrupt on
 }
 
-//external 1 interrupt edge trigger no need to clear the interrupt bit
+//external 1 interrupt level trigger no much we can do if the levle is still on 
 void ext1( void ) interrupt 2
 {
 	unsigned char c;
@@ -170,7 +235,7 @@ void ext1( void ) interrupt 2
 
 #if USE_MODULE2
 
-//external 0 interrupt edge trigger no need to clrea the interrupt bit
+//external 0 interrupt edge trigger no need to clear the interrupt bit
 void etx0( void ) interrupt 0
 {
 	EA = 0; //no interrupt me
@@ -190,4 +255,35 @@ void etx0( void ) interrupt 0
 }
 
 #endif
+
+
+#if USE_MODULE3
+
+//external 1 interrupt edge trigger no need to clear the interrupt bit
+void ext1( void ) interrupt 2
+{
+	EA = 0; //global interrupt off
+
+	mode = ~mode;
+
+	EA = 1; //global interrupt on
+}
+
+#endif
+
+
+//a delay function
+void delay( unsigned int ucMs )
+{
+//running at 11.0592MHz ?
+#define DELAYTIMES 239 
+
+	unsigned char ucCounter;
+
+	while( ucMs != 0 )
+	{
+		for( ucCounter=0; ucCounter < DELAYTIMES ; ucCounter ++ ){}
+		ucMs--;
+	}
+}
 
