839fc9c6480c3036b070dcc2b9876b4d5335529e
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-04-25 10:41:02 +0000

    test assemble language

--- trunk/QianRuShi/HomeWork/main.s
+++ trunk/QianRuShi/HomeWork/main.s
@@ -15,15 +15,15 @@ VAL			EQU 0AAH  ;赋值ＡＡＨ
 
 	ORG	100H
 MAIN:
-	SETB RS1
-	MOV SP,#4FH 
-	MOV R0,#DATA_ADDR	;数据块首地址DATA_ADDR 赋值给地址指针R0
-	MOV R2,#COUNT		;循环次数0AH付给计数器R2
-
-LOOP:
-	MOV @R0, #VAL		;给（R0）单元赋值VAL
-	INC R0				;地址指针R0加1
-	DEC	R2				;计算器R2减1
-	CJNE R2,#00H,LOOP	;判断R2是否为0，非0转LOOP继续请0
+	CLR C
+	CLR RS1
+	CLR RS0
+	MOV A,#38H
+	MOV R0,A
+	MOV 29H,R0
+	SETB RS0
+	MOV R1,A
+	MOV 26H,A
+	MOV 28H,C
 	SJMP $
 END
