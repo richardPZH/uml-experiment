3e1406fe9c707b66c9ed07e34ebb6343976dcc9b
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-05-20 05:50:32 +0000

    Add data mining stuff First version of Apriori, it's a stupid version...

---
+++ trunk/DataMining/Apriori.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   Apriori.cpp
+ * Author: IMS@SCUT
+ *
+ * Created on 2012年5月20日, 下午1:41
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int a;
+    
+    cin>>a;
+    
+    cout<<a<<"Hello world"<<endl;
+
+    return 0;
+}
+
