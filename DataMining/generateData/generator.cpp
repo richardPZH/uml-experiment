889bcddcc8a94be34acd8968895e86ea85de9a42
nemosail <nemosail@gmail.com>
2012-05-20 06:14:21 +0000

    why netbeans 7.1 not popup terminal?

--- DataMining/generateData/generator.cpp
+++ DataMining/generateData/generator.cpp
@@ -12,11 +12,28 @@
 
 using namespace std;
 
+#define PROGRAM_NAME "generator"
+#define G_FILE_NAME "itemSet"
 /*
  * 
  */
+void print_usage( void );
+
 int main(int argc, char** argv) {
+
+	if( argc < 3 )
+	{
+		print_usage();
+		exit( 1 );
+	}
+		
 
     return 0;
 }
 
+void print_usage( void )
+{
+	cerr<<PROGRAM_NAME<<" usage :\nThis is a transacton generator;\n";	
+	cerr<<PROGRAM_NAME<<" num_of_transaction max_length_of_each_row\n";
+	cerr<<"This version of generator will not generate duplicate items\n";
+}
