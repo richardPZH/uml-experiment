46f281d7bc56e72286a7ee151a11a5f5e8d27106
nemosail <nemosail@gmail.com>
2012-05-20 15:14:51 +0000

    open the itemSet file must use the absulate path because the hierachy of netbeans so...

--- DataMining/Apriori.cpp
+++ DataMining/Apriori.cpp
@@ -7,19 +7,43 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
+
+
+#define PROGRAM_NAME "Apriori"
+#define DATA_FILE "E:\\c\\DataMining\\generateData\\dist\\Debug\\Cygwin-Windows\\itemSet"
 
 using namespace std;
 
 /*
  * 
  */
+void print_usage( void );
+
 int main(int argc, char** argv) {
-    int a;
     
-    cin>>a;
+    if( argc < 2 )
+    {
+        print_usage();
+        exit( 1 );
+    }
     
-    cout<<a<<"Hello world"<<endl;
+    ifstream ifile;
+    ifile.open( DATA_FILE , ios::in );
+    //default is txt file right...but to me they are just squence of bytes
+    
+    if( !ifile )
+    {
+        cerr<<"Open file "<<DATA_FILE<<" Failed\n";
+        exit(1);
+    }
+    
 
     return 0;
 }
 
+void print_usage( void )
+{
+    cerr<<PROGRAM_NAME<<" usage:\n";
+    cerr<<PROGRAM_NAME<<" precent_of_support(e.g 30 40 60)\n";
+}
