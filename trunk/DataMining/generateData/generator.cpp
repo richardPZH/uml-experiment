b2e939cab141fcfeb6e4fbdbb1cd34029da7b788
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-05-20 05:55:50 +0000

    generate a random transaction.... initial import

---
+++ trunk/DataMining/generateData/generator.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   generator.cpp
+ * Author: IMS@SCUT
+ *
+ * Created on 2012年5月20日, 下午1:54
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
