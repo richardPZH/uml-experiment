3ccbba8024bcbc671b5cadd0131430b49dac68e7
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-06-07 10:17:26 +0000

    NormalizeKnn added

---
+++ trunk/PRProject/folder/NormalizeKnn.m
@@ -0,0 +1,106 @@
+function [time2classifeOneSample] = NormalizeKnn ( precent_of_training )
+
+load yeast.out
+
+%normal the rawData
+%map them to 0~1
+rawData = yeast(:,1:8)';
+rawData = mapminmax( rawData , 0 , 1);
+rawData = rawData';
+
+yeast = [ rawData yeast(:,9) ];
+
+
+%1~8 is attribute 9 is class label
+DS = dataset( yeast(:,1:8) , yeast(:,9));
+
+s = size( DS.labels );
+s = s(1);
+
+%disp( s );
+%disp( l );
+
+%training sample dataset TS
+%testing  sample dataset XS
+[ TS XS ] = gendat( DS , precent_of_training * s );
+s = size( XS.labels );
+s = s(1);
+
+%disp( ts.data );
+%disp( xs.data );
+
+%disp( 'Disping full dataset' );
+%figure( 1 );
+%scatterd(DS);
+%title( 'Full DataSet' );
+
+%disp( 'Disping training dataset');
+%figure( 2 );
+%scatterd(TS);
+%title( 'Training DataSet');
+
+%disp( 'Disping testing set');
+%figure( 3 );
+%scatterd(XS);
+%title('Testing DataSet');
+
+Xaccuracy = zeros( 15 , 1 );                %preallocate
+Taccuracy = zeros( 15 , 1 );
+tTTimeElasped = zeros( 15 , 1 );
+tXTimeElasped = zeros( 15 , 1 );
+tSTimeElasped = zeros( 15 , 2 );
+time2classifeOneSample = zeros( 15 ,1 );
+
+for k=1:15
+    tStart = tic;
+    classifier = knnc( TS ,k );                  %train a k classifier with Training dataset
+    tTTimeElasped(k) = toc( tStart );
+    
+    tStart = tic;
+    Xoutput = labeld( XS , classifier);      %get the teXting result
+    tXTimeElasped(k) = toc( tStart );
+    time2classifeOneSample( k , 1 ) = k;
+    time2classifeOneSample( k , 2 ) = tXTimeElasped(k) / s;
+    
+    len = size( Xoutput );
+    len = len( 1 );
+    j = 0;
+    for i = 1:len
+        if( Xoutput(i) == XS.labels(i) )
+            j = j + 1;
+        end
+    end
+           
+    Xaccuracy(k) = j * 1.0 / len;
+    
+    tStart = tic;
+    Toutput = labeld( TS , classifier);      %get the Training resullt
+    tSTimeElasped(k) = toc( tStart );
+    
+    len = size( Toutput );
+    len = len(1);
+    j = 0;
+    for i = 1:len
+        if( Toutput(i) == TS.labels(i) )
+            j = j + 1;
+        end
+    end
+           
+    Taccuracy(k) = j * 1.0 / len;
+    
+end
+
+k = 1:1:15;
+
+h = figure;
+%title([num2str(precent_of_training * 100) '% of trainning' ]);
+bigTitle = [num2str(precent_of_training * 100) '% of trainning' ];
+set(h,'name',bigTitle,'Numbertitle','off');
+
+subplot(2,1,1),plot( k , Taccuracy , 'r-.+' , k , Xaccuracy , 'b-o' ),grid on ,ylabel('Accuracy'),xlabel('K'),title('Testing Accuracy of Precentage KNN classifier(Red is trainging acc Blue is testing acc');
+subplot(2,1,2),plot( k , tTTimeElasped , 'r' , k , tXTimeElasped , 'g' , k , tSTimeElasped , 'b' ), grid on , xlabel('K') , ylabel('time elasped in sec') , title('Red is traing time; Green is label testing set time; Blue is training set time');
+
+
+
+
+end
