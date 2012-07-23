595a35376be66c2c344b912caaf924620a1fc6d9
nemo.sail@gmail.com <nemo.sail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-07-23 12:37:08 +0000

    size_t is different from what i think... size_t - size_t returns size_t is non negative

--- trunk/BoostedSearchForest/BSF.h
+++ trunk/BoostedSearchForest/BSF.h
@@ -20,11 +20,11 @@ public:
     BSF();
     virtual ~BSF();
 private:
-    size_t m;                //there are m trees in the forest here m must >= 0
-    Tree * forestEntrance;   //Entrance of the forest, in fact is a place where trees reside
-    Mat< char > * p_s;         //a pointer to the similarity matrix [ s11 s12 ... s1n ; s21 s22 .. s2n ; .... ; sn1 sn2 ... snn ] the similarity matrix's diagno is 1 and symmetrical
-    Mat< double > * p_x;       //a pointer to the original sample matrix [ x11 x12 x13 .. x1k ; x21 x22 x23 ..x2k ; ... ; xn1 xn2 xn3 ... xnk ]
-
+    size_t m;                        //there are m trees in the forest here m must >= 0 so size_t
+    double lamda;                    //the λ the tuning paramemter, balances the retrieval quality and computational cost, need to ask the author Zhen Li
+    Tree * forestEntrance;           //Entrance of the forest, in fact is a place where trees reside
+    const Mat< char > * p_s;         //a pointer to the similarity matrix [ s11 s12 ... s1n ; s21 s22 .. s2n ; .... ; sn1 sn2 ... snn ] the similarity matrix's diagno is 1 and symmetrical
+    const Mat< double > * p_x;       //a pointer to the original sample matrix [ x11 x12 x13 .. x1k ; x21 x22 x23 ..x2k ; ... ; xn1 xn2 xn3 ... xnk ]
 };
 
 #endif	/* BSF_H */
