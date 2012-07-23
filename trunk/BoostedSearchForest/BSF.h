9c7bcf27674605854fd72c7494f2933c8f9ce2f1
nemo.sail@gmail.com <nemo.sail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-07-23 09:16:02 +0000

    initial import This project is build on Fedora14 netbeans g++ with Armadillo. May or May not Finishe the whole story.
    Thanks to:
    
    Conrad Sanderson.
    Armadillo: An Open Source C++ Linear Algebra Library for Fast Prototyping and Computationally Intensive Experiments.
    Technical Report, NICTA, 2010.

---
+++ trunk/BoostedSearchForest/BSF.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   BSF.h
+ * Author: alpha
+ *
+ * Created on July 23, 2012, 8:49 AM
+ */
+
+#ifndef BSF_H
+#define	BSF_H
+
+class BSF {
+public:
+    BSF();
+    BSF(const BSF& orig);
+    virtual ~BSF();
+private:
+
+};
+
+#endif	/* BSF_H */
+
---
+++ trunk/BoostedSearchForest/Tree.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Tree.cpp
+ * Author: alpha
+ * 
+ * Created on July 23, 2012, 8:54 AM
+ */
+
+#include "Tree.h"
+
+Tree::Tree() {
+}
+
+Tree::Tree(const Tree& orig) {
+}
+
+Tree::~Tree() {
+}
+
