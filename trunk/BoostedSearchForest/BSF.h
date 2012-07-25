f8409a7fffff7cc53a48751f7634a2e1c670b1e3
nemo.sail@gmail.com <nemo.sail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-07-25 04:08:30 +0000

    Gotta work!!

--- trunk/BoostedSearchForest/BSF.h
+++ trunk/BoostedSearchForest/BSF.h
@@ -17,14 +17,16 @@ using namespace arma;
 
 class BSF {
 public:
-    BSF();
+    BSF( const Mat< double > * cp_x , const Mat< char > * cp_s , const double clamda , const size_t cm );
     virtual ~BSF();
+    bool boost( void );
 private:
     size_t m;                        //there are m trees in the forest here m must >= 0 so size_t
     double lamda;                    //the λ the tuning paramemter, balances the retrieval quality and computational cost, need to ask the author Zhen Li
     Tree * forestEntrance;           //Entrance of the forest, in fact is a place where trees reside
-    const Mat< char > * p_s;         //a pointer to the similarity matrix [ s11 s12 ... s1n ; s21 s22 .. s2n ; .... ; sn1 sn2 ... snn ] the similarity matrix's diagno is 1 and symmetrical
-    const Mat< double > * p_x;       //a pointer to the original sample matrix [ x11 x12 x13 .. x1k ; x21 x22 x23 ..x2k ; ... ; xn1 xn2 xn3 ... xnk ]
+    const Mat< char > * p_s;         //a pointer to the similarity matrix [ s00 s01 ... s0n ; s10 s11 .. s1n ; .... ; sn0 sn1 ... snn ] the similarity matrix's diagno is 1 and symmetrical
+    const Mat< double > * p_x;       //a pointer to the original sample matrix [ x0f0 x0f1 x0f2 .. x0fk ; x1f0 x1f1 x1f2 ..x1fk ; ... ; xnf0 xnf1 xnf2 ... xnfk ]
+    Mat< double > * p_w;             //weight vector [w00 w01 w02 ...; w10 w11 w12..... wn0 wn1 .. 2nn ]
 
 
 };
