3b8c5a300f8f3199c7646b3f97f49f145987200a
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-09-22 07:32:41 +0000

    the transform a row( a image ) of the CIFAR dataset into human readable picture. It works fine, but need more improve!

--- trunk/Hierachical/ShowCIFAR.m
+++ trunk/Hierachical/ShowCIFAR.m
@@ -10,3 +10,21 @@ function [ ] = ShowCIFAR( X )
 
 %check the X dimension
 [ n d ] = size( X );
+
+% d should be 1024
+d = d / 3;          
+
+% XX is just a row vector [ x1 x2 ... xd ];
+XX = X( 1 , : );   
+
+Img = [ reshape( XX( 1:d ) , 32 , 32 ) reshape( XX( d+1:d*2 ) , 32 , 32 ) reshape( XX( 2*d+1:end ) , 32 , 32 ) ];
+Img = [ Img( : , 1:32 )' Img( : , 33:64 )' Img( : , 65:end )' ];
+Img = reshape( Img , 32 , 32 ,3 );
+
+% Using image() to show the image to human
+image( Img );
+
+
+
+
+
