d212c938ba2856ba43cbe1f3e92c03337dbf9def
nemosail <nemosail@gmail.com>
2012-09-22 07:51:48 +0000

    Ready to show multiply image

--- Hierachical/ShowCIFAR.m
+++ Hierachical/ShowCIFAR.m
@@ -10,24 +10,27 @@ function [ ] = ShowCIFAR( X )
 
 %check the X dimension
 [ n d ] = size( X );
-
+    
 % d should be 1024
-d = d / 3;          
+d = d / 3;   
 
-% XX is just a row vector [ x1 x2 ... xd ];
-XX = X( 1 , : );   
+for i=1:n
 
-Img = [ reshape( XX( 1:d ) , 32 , 32 ) reshape( XX( d+1:d*2 ) , 32 , 32 ) reshape( XX( 2*d+1:end ) , 32 , 32 ) ];
-Img = [ Img( : , 1:32 )' Img( : , 33:64 )' Img( : , 65:end )' ];
-Img = reshape( Img , 32 , 32 ,3 );
+    % XX is just a row vector [ x1 x2 ... xd ];
+    XX = X( i , : );   
 
-% Using image() to show the image to human
-[ r , c , d ] = size( Img );
-figure( 'Units' , 'Pixels' , 'Position' , [100 100 c r ] );
-image( Img );
-axis image off;
-set( gca , 'Position' , [0 0 1 1 ]);
+    Img = [ reshape( XX( 1:d ) , 32 , 32 ) reshape( XX( d+1:d*2 ) , 32 , 32 ) reshape( XX( 2*d+1:end ) , 32 , 32 ) ];
+    Img = [ Img( : , 1:32 )' Img( : , 33:64 )' Img( : , 65:end )' ];
+    Img = reshape( Img , 32 , 32 ,3 );
 
+    % Using image() to show the image to human
+    [ m , n , o ] = size( Img );
+    figure( 'Units' , 'Pixels' , 'Position' , [100 100 m n ] );
+    image( Img );
+    axis image off;
+    set( gca , 'Position' , [0 0 1 1 ]);
+
+end
 
 
 
