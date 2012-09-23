e388e760c6eb873190b29690eed419bd9442c0a1
nemosail <nemosail@gmail.com>
2012-09-23 13:49:07 +0000

    Level2Hash.m is add, but it need more test... Level3Hash.m is add, but it is empty. need to finish it..

---
+++ Hierachical/ITQSen.m
@@ -0,0 +1,41 @@
+function [B,R] = ITQSen( X , W , n_iter)
+%
+% main function for ITQ Sensitivity which finds a rotation of the PCA embedded data
+% Input:
+%       X: n*d raw data, n is the number of images and d is dimension
+%       W: d*c projection matrix, 
+%       n_iter: max number of iterations, 50 is usually enough
+% Output:
+%       B: n*c binary matrix
+%       R: the c*c rotation matrix found by ITQ
+% Author:
+%       IMS@SCUT
+%       Yes, We can plot the J(R) to see how much iterations should we
+%       take!
+%       
+%
+
+
+V = X * W;
+
+% initialize with a orthogonal random rotation
+bit = size(V,2);
+R = randn(bit,bit);
+[U11 S2 V2] = svd(R);
+R = U11(:,1:bit);
+
+% ITQ to find optimal rotation, R is orthogonal !! I am wrong!
+for iter = 1 : n_iter
+    Z = V * R;      
+    UX = ones(size(Z,1),size(Z,2)).*-1;
+    UX(Z>=0) = 1; 
+    D = V' * UX;
+    
+    G = sqrtm( D * D' );
+    R = inv( G ) * D;       % change inv(D) * G into inv(G)*D
+    
+end
+
+% make B binary
+B = UX;
+B(B<0) = 0;
