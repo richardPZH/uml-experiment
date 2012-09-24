34a396f8913502bb42b5d913c20d750c6c63ea69
nemosail <nemosail@gmail.com>
2012-09-24 06:21:59 +0000

    add more hash at level 1 , think clearly

---
+++ Hierachical/Level1Hash2.m
@@ -0,0 +1,10 @@
+function [ ] = Level1Hash2( )
+% This function provide a second choice of the first level hashing
+% Well, how many bits should we assign to the first level, 1bit, 2bit , 3?
+%
+% Input:
+%
+%
+% Author:
+%     IMS@SCUT Once 2012/09/24
+
