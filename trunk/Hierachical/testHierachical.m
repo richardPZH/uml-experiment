bb19073bec68bbcaba391f50ac92d1d8746f3aaf
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-09-25 07:14:08 +0000

    use the unique function to get the entrance ready!\ help unique to see how.. [ b i j ] = unique( X , 'rows' ); X(i) = b; b(j) = X; find( j == 1 );

--- trunk/Hierachical/testHierachical.m
+++ trunk/Hierachical/testHierachical.m
@@ -48,6 +48,22 @@ telabels = labels( R );
 % level 1
 [W0 R0 centerPoint0] = Level1Hash2( trGist , trlabels , firstBit , 'ITQ' );
 
+%Calculate the J(R,1) and store the buckets
+XX = X - repmat( centerPoint0 , size( X , 1 ) , 1 );
+XX = XX * W0 * R0;
+XX( XX >= 0 ) = 1;
+XX( XX <  0 ) = 0;
+
+[ b i j ] = unique( XX , 'rows' );
+Entrance1 = cell( size( b , 1 ) , 2 );
+
+for m = 1 : size( b , 1 )
+	Entrance1{ m , 1 } = b( m , :);
+	Entrance1{ m , 2 } = find( j == m );
+end
+
+
+
 % level 2 %This traing is different, This should train those bucket found in Level1 !!!
 
 % level 3
