a28be67b572fdb1adfe7299aad25dac2714dfe2b
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-09-25 14:15:54 +0000

    I am fooled!! What am I doing? Even the mighty God doesn't know!!

--- trunk/Hierachical/testHierachical.m
+++ trunk/Hierachical/testHierachical.m
@@ -92,10 +92,50 @@ for m = 1 : size( Entrance1 , 1 )
 end
 
 
-% level 3
+% level 3 , at level 3 , we assume all the images are in the same class
+% we split a little bit more, hope that return images will more like the input! 
+%
 % This implementation of Hierachical Hashing is bad bad bad!! Can We Do Better?
 % Things are getting out of my control, dota it!
-Entrance3 = cell( 
+Entrance3 = cell( size( Entrance2 , 1 ) , 1 );
+
+for m = 1 : size( Entrance2 , 1 )
+	L2Gist = trGist( Entrance1{ m , 1 } , : );
+
+	anoymousEntrance = Entrance( m , 2 );
+
+	L3cell = cell( size( anoymousEntrance , 1 ) , 1 );
+
+	for n = 1 : size( anoymousEntrance , 1 )
+
+		L3Gist = L2Gist( anoymousEntrance{ n , 2 } , : );
+
+		[ W2 R2 centerPoint2 ] = Level3Hash( L3Gist , thirdBit , 'OURSITQ' );
+
+		% need to Calculate the J(R,1) and store the buckets
+		XX = L3Gist - repmat( centerPoint2 , size( L3Gist , 1 ) , 1 );
+		XX = XX * W2 * R2;
+		XX( XX >= 0 ) = 1;
+		XX( XX <  0 ) = 0;
+
+
+		[ b i j ] = unique( XX , 'rows' );
+
+		tmpCell = cell( size( b , 1) , 2 );
+		for o = 1 : size( b , 1)
+			tmpCell{ o , 1 } = b( n , :);
+			tmpCell{ o , 1 } = find( j == o );
+		end
+
+		L3cell{ n , 1 } = tmpCell;
+
+
+	end
+
+	Entrance3{ m , 1 } = L3cell;
+
+
+end
 
 
 
