6766c6c0e98b4d29c704cd4ec94362600aa84c61
nemosail <nemosail@gmail.com>
2012-09-26 07:24:52 +0000

    fixbug

--- Hierachical/getEntrance1.m
+++ Hierachical/getEntrance1.m
@@ -1,17 +1,35 @@
-function [W0 R0 centerPoint0 Entrance1 ] = getEntrance1( trGist , trlabels , firstBit , method )
+function [W0 R0 cP0 E1 ] = getEntrance1( trGist , trlabels , bit , method )
 % 
-%  
+% This function performs the first level hashing, its purpose is to roughtly group 
+% the general class together.  e.g. 
+%     living thing vs. nonliving thing;
+%     things on land / on ocean / in the air
 %
-[W0 R0 centerPoint0] = Level1Hash2( trGist , trlabels , firstBit , method );
+% Input:
+%     trGist, the nx320 CIFAR , Gist represented images as a learning samples
+% 	  trlabels, the nx1 CIFAR labels, 0~9
+%     bit, how many bit is assigned to this hash code
+%     method, e.g 'ITQ' 'OURSITQ' 'ITQRR'
+%
+% Output:
+%     W0 , the projection matrix found using the CCA
+%     R0 , the orthogonal matrix found by ITQ like algorithm
+%     cP0 , centerPoint of the training samples , usefully when searching
+%     E1 , the Entrance table for Level1 , read your design!!
+
+
+[W0 R0 cP0] = Level1Hash2( trGist , trlabels , bit , method );
 
 %need to Calculate the J(R,1) and store the buckets
+
 XX = trGist - repmat( centerPoint0 , size( trGist , 1 ) , 1 );
 XX = XX * W0 * R0;
 XX( XX >= 0 ) = 1;
 XX( XX <  0 ) = 0;
 
 [ b i j ] = unique( XX , 'rows' );
-Entrance1 = cell( size( b , 1 ) , 2 );
+
+E1 = cell( size( b , 1 ) , 2 );
 
 for m = 1 : size( b , 1 )
 	Entrance1{ m , 1 } = b( m , :);
