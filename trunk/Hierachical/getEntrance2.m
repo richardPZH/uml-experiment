010c5a1b68516311d85d40e21a02bb3b1f486858
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-09-26 07:49:36 +0000

    fixbug and reconstruct my m files ...

--- trunk/Hierachical/getEntrance2.m
+++ trunk/Hierachical/getEntrance2.m
@@ -1,19 +1,37 @@
-function [ Entrance2 ] = getEntrance2( Entrance1 , trGist , trlabels , secondBit , method )
+function [ Entrance ] = getEntrance2( E1 , trGist , trlabels , bit , method )
+%
+% This function performs the second hashing, its purpose is to hash the hashed 
+% general class into isolated classes. e.g.
+%      living things -> bird cat deer dog horse frog
+%      things on in the air -> bird airplane 
+%
+% Input:
+%     E1, the first level Entrance , it is a cell( n , 2 ) , cell( x , 1 ) stores
+%         the hashcode, cell( x , 2 ) stores index of the trGist
+%     trGist, the nx320 CIFAR , Gist represented images as a learning samples
+% 	  trlabels, the nx1 CIFAR labels, 0~9
+%     bit, how many bit is assigned to this hash code
+%     method, e.g 'ITQ' 'OURSITQ' 'ITQRR'
 %
 %
+% Output:
+%     Entrance -> the second levle Entrance, it is a cell( n , 2 ) , 
+%         cell( x , 1 ) stores the w1 r1 cp1 of the E1{ x , : }
+%         cell( x , 2 ) stores a cell( n , 2 )
 %
-%
-% This training is different, This should train those bucket found in Level1 !!!
 
-Entrance2 = cell( size( Entrance1 , 1 ) , 2 );
+Entrance = cell( size( E1 , 1 ) , 2 );
 
-for m = 1 : size( Entrance1 , 1 )
-	tmpGist = trGist( Entrance1{ m , 2} , : );
-	tmplabels = trlabels( Entrance1{ m , 2 } );
-    [W1 R1 centerPoint1] = Level2Hash(tmpGist,tmplabels,secondBit,method);
+for m = 1 : size( E1 , 1 )
+
+	tmpGist = trGist( E1{ m , 2} , : );
+
+	tmplabels = trlabels( E1{ m , 2 } );
+
+    [ W1 R1 cP ] = Level2Hash( tmpGist , tmplabels , bit , method );
 
 	% need to Calculate the J(R,1) and store the buckets
-	XX = tmpGist - repmat( centerPoint1 , size( tmpGist , 1 ) , 1 );
+	XX = tmpGist - repmat( cP , size( tmpGist , 1 ) , 1 );
 	XX = XX * W1 * R1;
 	XX( XX >= 0 ) = 1;
 	XX( XX <  0 ) = 0;
@@ -24,10 +42,13 @@ for m = 1 : size( Entrance1 , 1 )
 
 	for n = 1 : size( b , 1 )
 		anoymousEntrance{ n , 1 } = b( n , : );
-		anoymousEntrance{ n , 2 } = Entrance1{ m , 2 }( find( j == n ) ); %Index of trGist
+
+		%Index of trGist % This code I used find and matlab ask me to use
+		%the logical expression j == n to avoid vector generation?? note!
+		anoymousEntrance{ n , 2 } = E1{ m , 2 }(  j == n  ); 
 	end
 
-	Entrance2{ m , 1 } = { W1 , R1 , centerPoint1 };
-	Entrance2{ m , 2 } = anoymousEntrance;
+	Entrance{ m , 1 } = { W1 , R1 , cP };
+	Entrance{ m , 2 } = anoymousEntrance;
 
 end
