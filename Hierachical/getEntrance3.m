32287fd800e7353ab7066a189b1d5227fabcb56b
nemosail <nemosail@gmail.com>
2012-09-26 13:43:24 +0000

    Heavy bit... I was wrong at first, so the storm has taken away everything!! 沉重的教训，没有为后来的查找考虑好就开始设计结构，使得为了后来查找的方便，前面的工作都浪费了不少啊！！！

--- Hierachical/getEntrance3.m
+++ Hierachical/getEntrance3.m
@@ -8,9 +8,10 @@ function [ Entrance ] = getEntrance3( E2 , trGist , bit , method )
 % We split a little bit more, hope that return images will more like the input! 
 %
 % Input:
-%     E2, the second level Entrance , it is a cell( n , 2 )
+%     E2, the second level Entrance , it is a cell( n , 3 )
 %         the cell( x , 1) stores the [w1 r1 cp1]
-%         the cell( x , 2) stores a cell( n , 2 )
+%         the cell( x , 2) stores the binary code matrix
+%         the cell( x , 3) stores a cell that indices of the trGist
 %     trGist, the nx320 CIFAR , Gist represented images as a learning samples
 % 	  trlabels, the nx1 CIFAR labels, 0~9
 %     bit, how many bit is assigned to this hash code
@@ -19,9 +20,10 @@ function [ Entrance ] = getEntrance3( E2 , trGist , bit , method )
 %
 % Output:
 %     Entrance -> the third level Entrance , is is a cell( n ,1 )
-%         the cell( x , 1 ) stores a L3cell -> cell( n , 2 )
+%         the cell( x , 1 ) stores a L3cell -> cell( n , 3 )
 %         L3cell( x , 1 ) stores w2 r2 cp2
-%         L3cell( x , 2 ) stores a cell( n , 2 )
+%         L3cell( x , 2 ) stores the binary code matrix
+%         L3cell( x , 3 ) stores a cell( n , 1 ) of the trGist indice
 %         
 %
 %
@@ -33,13 +35,13 @@ Entrance = cell( size( E2 , 1 ) , 1 );
 
 for m = 1 : size( E2 , 1 )
 
-	anoymousEntrance = E2( m , 2 );
+	anoymousEntrance = E2( m , 3 );
 
-	L3cell = cell( size( anoymousEntrance , 1 ) , 2 );
+	L3cell = cell( size( anoymousEntrance , 1 ) , 3 );
 
 	for n = 1 : size( anoymousEntrance , 1 )
 
-		L3Gist = trGist( anoymousEntrance{ n , 2 } , : );
+		L3Gist = trGist( anoymousEntrance{ n } , : );
 
 		[ W2 R2 cP ] = Level3Hash( L3Gist , bit , method );
 
@@ -52,17 +54,18 @@ for m = 1 : size( E2 , 1 )
 
 		[ b i j ] = unique( XX , 'rows' );
 
-		tmpCell = cell( size( b , 1) , 2 );
+		tmpCell = cell( size( b , 1) , 1 );
+
 		for o = 1 : size( b , 1)
-			tmpCell{ o , 1 } = b( n , : );
 
 			% think carefully this index the trGist and trlabel! why not find( j == 0 )
-			tmpCell{ o , 2 } = anoymousEntrance{ n , 2 }(  j == o  ); 
+			tmpCell{ o , 2 } = anoymousEntrance{ n }(  j == o  ); 
 		end
 
 		
 		L3cell{ n , 1 } = [ W2 , R2 , cP ];
-		L3cell{ n , 2 } = tmpCell;
+		L3cell{ n , 2 } = b;
+		L3cell{ n , 3 } = tmpCell;
 
 
 	end
