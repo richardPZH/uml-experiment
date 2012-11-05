09558254683036c3b5b859d0c2db3d59736b88da
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-11-05 03:43:06 +0000

    add the Level2Search.m to make the whole search clean... need more idea!!!

--- trunk/Hierachical/searchImage2.m
+++ trunk/Hierachical/searchImage2.m
@@ -1,4 +1,4 @@
-function [ recall precision ] = searchImage( inGist , inLabel , inVector , trGist , trLabels , trVector , E1 , E2 , E3 )
+function [ recall precision ] = searchImage2( inGist , inLabel , inVector , trGist , trLabels , trVector , E1 , E2 , E3 )
 %
 % This function use the testing image to evaluate the performance 
 % of the Hierachical hashing method... 
@@ -19,7 +19,60 @@ function [ recall precision ] = searchImage( inGist , inLabel , inVector , trGis
 %     precision: average precision of ...
 %
 % Authors:
-%     IMS@SCUT Once 2012/11/05
+%     IMS@SCUT Once 2012/11/04
 %
 % Advices:
 %     How to use more vector operation rather than for loop
+
+
+recall = 0;
+precision = 1;
+
+% the hamming distance in level 1, which we need to perdefine
+L1Dis = 2;
+
+%get the first level hash code
+W0=  E1{ 1 , 1 }{ 1 };
+R0 = E1{ 1 , 1 }{ 2 };
+cP0 = E1{ 1 , 1 }{ 3 };
+
+% first level hash
+XX = inGist - repmat( cP0 , size( inGist , 1 ) , 1 );
+
+XX = XX * W0 * R0;
+
+XX( XX >= 0 ) = 1;
+XX( XX <  0 ) = 0;
+
+% In the Entrace 1 , E1{1,2} is the binary code matrix, one row, one group images
+L1Code = E1{ 1 , 2 };
+
+for numOfImage = 1 : size( inLabel , 1 )
+   
+    imageGist = inGist( numOfImage , : );     %search image in gist
+    imageL1Code = XX( numOfImage , : );       %search image 1st code
+    imageVector = inVector( numOfImage , : ); %search image in RGB
+    imageLabel = inLabel( numOfImage );       %search image label
+    
+    %search one image starts
+    hmd = CalHammingDist( imageL1Code , L1Code , 'vec' );
+    
+    for dist = 0 : L1Dis 
+        % handle distance of dist starts
+        vec = find( hmd == dist );
+        
+        for a = 1 : size( vec , 1 )
+            
+            [ recall precision ] = Level2Search( recall , precision , imageGist , imageVector , imageLabel , trGist , trLabels , trVector , vec( a ) , E2 , E3 );
+            
+        end
+        
+        % handle distance of dist ends
+    end
+    
+    
+    
+    %search one image finishes
+    
+end
+
