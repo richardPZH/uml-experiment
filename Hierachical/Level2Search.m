9956f883223768f86e8db4c6c40e3216591a7ff0
nemosail <nemosail@gmail.com>
2012-11-06 07:13:51 +0000

    add Level3Search.m and some global variables

--- Hierachical/Level2Search.m
+++ Hierachical/Level2Search.m
@@ -1,10 +1,13 @@
-function [ recall precision ] = Level2Search( recall , precision , imageGist , imageVector , imageLabel , trGist , trLabels , trVector , index , E2 , E3 )
+function [ ] = Level2Search( imageGist , imageVector , imageLabel , trGist , trLabels , trVector , index , E2 , E3 )
 %
 % This function will search into the second level 
 %
 %
 % 
 % IMS@SCUT Once 2012/11/05
+
+% the hamming distance in level 2, which we need to perdefine
+L2Dis = 3;
 
 % Get Level 2 info
 W1=  E2{ index , 1 }{ 1 };
@@ -17,5 +20,20 @@ L2Code = E2{ index , 2 };
 code = ( imageGist - cP1  ) * W1 * R1;
 code( code >= 0 ) = 1;
 code( code <0   ) = 0;
+
+hmd = CalHammingDist( code , L2Code , 'vec' );
+
+for k = 0 : L2Dis
+   %level 2 distance from 0 to L2Dis
+   vec = find( hmd == k );
+   
+   for a = 1 : size( vec , 1 )
+       
+        Level3Search( imageGist , imageVector , imageLabel , trGist , trLabels , trVector , index , vec( a ) , E3 );
+       
+   end
+   
+   %level 2 end
+end
 
 
