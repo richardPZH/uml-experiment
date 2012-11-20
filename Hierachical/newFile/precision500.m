b39d327792aa8ddf93593d02a308eda906d99df8
nemosail <nemosail@gmail.com>
2012-11-20 12:57:54 +0000

    add the plot file precision500.m Average precision for top 500 retrieved images for supervised data.

---
+++ Hierachical/newFile/precision500.m
@@ -0,0 +1,15 @@
+function [] = precision500( recall , precision )
+% 
+% This function is used to plot the precision@500 
+% Input:
+%      recall 
+%      precision
+%
+%
+
+% Because the numOf samples is 5000
+numOfSam = 5000;
+
+numOfRet = ( numOfSam .* recall ) ./ precision ;
+
+plot( numOfRet , precision , '-x' );
