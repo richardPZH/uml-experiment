2798a098cc40e63856c7e5f2ce5578d0f515bad1
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-11-20 14:10:02 +0000

    fixbugs

--- trunk/Hierachical/newFile/precision500.m
+++ trunk/Hierachical/newFile/precision500.m
@@ -10,6 +10,7 @@ function [] = precision500( recall , precision )
 % Because the numOf samples is 5000
 numOfSam = 5000;
 
-numOfRet = ( numOfSam .* recall ) ./ precision ;
+numOfRet = ( numOfSam * recall ) ./ precision ;
 
 plot( numOfRet , precision , '-x' );
+grid on;
