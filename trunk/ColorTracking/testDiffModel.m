eee8f4f8dc6f6513374b8283dd18b2ed4e137ac8
nemosail@gmail.com <nemosail@gmail.com@5ae8381d-a648-dfd8-51b4-8ac112e361b6>
2012-12-17 08:44:29 +0000

    This version works quite well. tk = Tracker( 'sphere' , capturer , 1.5 );  %the avg*1.5sphere is too big
    %This ellipsoid works quite well!
    tk = Tracker( 'ellipsoid' , capturer , 1.2 , 0.5 , 0.5 , 0.7 );
    
    %This cylinder works quite well!
    tk = Tracker( 'cylinder' , capturer , 1.2 , 10 );

--- trunk/ColorTracking/testDiffModel.m
+++ trunk/ColorTracking/testDiffModel.m
@@ -7,7 +7,7 @@ function [] = testDiffModel()
 
 info = imaqhwinfo
 winfo = imaqhwinfo('winvideo')
-obj = videoinput( 'winvideo' , 1 ,  'RGB24_320x240' ); %'YUY2_320x240' );
+obj = videoinput( 'winvideo' , 1 ,  'YUY2_320x240' ); %'RGB24_320x240' );
 himage = preview( obj );
 video = figure(1);
 set( obj , 'ReturnedColorSpace' , 'rgb' );
@@ -17,19 +17,19 @@ capturer = getCapturer( obj , 'getpts' );
 
 %sphere paramater:
 %                                   %varargin{ 3 } may be : 'a' 'b' 'c' 'avg' preOfavg 
-tk = Tracker( 'sphere' , capturer , 0.4 ); 
+tk = Tracker( 'sphere' , capturer , 1.5 ); 
 tk.showBoundary();
-showSupportSpace( obj , tk , 1 );
+showSupportSpace( obj , tk , 1 );  % third 1 is the step
 %
 
 %ellipsoid paramater:               %varargin{ 3 4 5 6} should be: scale of a b c, and di
-tk = Tracker( 'ellipsoid' , capturer , 1 , 0.4 , 0.4 , 0.4 );
+tk = Tracker( 'ellipsoid' , capturer , 1.2 , 0.5 , 0.5 , 0.7 );
 tk.showBoundary();
 showSupportSpace( obj , tk , 1 );
 %
 
 %cylinder paramater:                %varargin{ 3 4 } should be : scale of a, di
-tk = Tracker( 'cylinder' , capturer , 1.2 , 8 );
+tk = Tracker( 'cylinder' , capturer , 1.2 , 10 );
 tk.showBoundary();
 showSupportSpace( obj , tk , 1);
 %
