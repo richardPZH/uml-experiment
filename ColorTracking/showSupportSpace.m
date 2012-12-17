31396246c2c70725701cf5e03cfa443b48141e26
nemosail <nemosail@gmail.com>
2012-12-17 08:26:17 +0000

    need to debug and find a better parameter...!! we have many parameters!!

--- ColorTracking/showSupportSpace.m
+++ ColorTracking/showSupportSpace.m
@@ -1,14 +1,17 @@
-function [] = showSupportSpace( obj ,  tk ) 
+function [] = showSupportSpace( obj ,  tk , step ) 
 
 % The showSupportSpace is used to evaluate the different tk performance
 
+% default argument step
+if nargin < 3
+    step = 8;
+end
+
 frame = getsnapshot( obj );
 
-[row col] = size( frame );
+[row col color] = size( frame ); %always remember a RGB image is mxnxl
 
 nframe = zeros( row , col , 'uint8' );
-
-step = 8;
 
 for m = 1 : step : row
     for n = 1 : step : col
@@ -16,7 +19,7 @@ for m = 1 : step : row
         
         x = [ tmp(1) ; tmp(2); tmp(3) ];
         
-        nframe = uint8( 255 * tk.isTargetColor( x ) );     
+        nframe( m , n ) = uint8( 255 * tk.isTargetColor( x ) );     
                 
     end
 end
