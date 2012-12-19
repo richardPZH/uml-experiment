242f5765fa93054da4e0033893af92ef4598db31
nemosail <nemosail@gmail.com>
2012-12-19 14:11:44 +0000

    we saved a green to test performance!!

--- ColorTracking/testDiffModel.m
+++ ColorTracking/testDiffModel.m
@@ -13,43 +13,49 @@ video = figure(1);
 set( obj , 'ReturnedColorSpace' , 'rgb' );
 
 %getCapturer paramaters:           %'default_red' | 'getpts' | 'getrect'
-capturer = getCapturer( obj , 'getpts' );
+%capturer = getCapturer( obj , 'getpts' );
 
+%This read the same data
+%
+load 'green.mat'     % get data , a mx3 [ r g b ] vector
+        
+capturer.location = [ 128 , 128 ];  %give this initial location is row=128,col=128
+capturer.data = data;
+%
+%end reading data
 frame = getsnapshot( obj );
 
-%sphere paramater:
+% %sphere paramater:
 %                                   %varargin{ 3 } may be : 'a' 'b' 'c' 'avg' preOfavg 
 % tk = Tracker( 'sphere' , capturer , 0.6 ); 
 % tk.showBoundary();
-%%showSupportSpace( frame , tk , 1 );  % third 1 is the step
-
-%We show how the machine see the world
-%
+% %showSupportSpace( frame , tk , 1 );  % third 1 is the step
+% %We show how the machine see the world
 % while( 1 )
 %     frame = getsnapshot( obj );
 %     showSupportSpace( frame , tk , 7 );
 % end
-%
+
 
 %ellipsoid paramater:               %varargin{ 3 4 5 6} should be: scale of a b c, and di
-% tk = Tracker( 'ellipsoid' , capturer , 1.3 , 0.7 , 0.7 , 0.7 );
-% tk.showBoundary();
-% %showSupportSpace( frame , tk , 1 );
-% while( 1 )
-%     frame = getsnapshot( obj );
-%     showSupportSpace( frame , tk , 7 );
-% end
-%
-%
-
-%cylinder paramater:                %varargin{ 3 4 } should be : scale of a, di
-tk = Tracker( 'cylinder' , capturer , 1.2 , 9 );
+tk = Tracker( 'ellipsoid' , capturer , 1.3 , 0.7 , 0.7 , 0.7 );
 tk.showBoundary();
 %showSupportSpace( frame , tk , 1 );
 while( 1 )
     frame = getsnapshot( obj );
     showSupportSpace( frame , tk , 7 );
 end
+%
+%
+
+%cylinder paramater:                %varargin{ 3 4 } should be : scale of a, di
+% tk = Tracker( 'cylinder' , capturer , 1.2 , 9 );
+% tk.showBoundary();
+% %showSupportSpace( frame , tk , 1 );
+% while( 1 )
+%     frame = getsnapshot( obj );
+%     showSupportSpace( frame , tk , 7 );
+% end
 %
 
 
